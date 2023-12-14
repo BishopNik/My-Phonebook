0bec6439d2011237398629e51fa9bb7d02d50444
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-12-14 22:13:00 +0100

    add auth0 provider

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { GlobalStyle } from 'components/Helpers/GlobalStyle';
 import { store, persistor } from './redux/store';
 import 'modern-normalize';
 import { Toaster } from 'react-hot-toast';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const theme = {
 	spacing: x => `${x * 4}px`,
@@ -21,22 +22,30 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<BrowserRouter basename='/My-Phonebook'>
-					<ThemeProvider theme={theme}>
-						<App />
-						<Toaster
-							position='top-right'
-							reverseOrder={false}
-							gutter={8}
-							toastOptions={{
-								duration: 5000,
-								style: {
-									background: '#fdfbea',
-									color: '#000000',
-								},
-							}}
-						/>
-						<GlobalStyle />
-					</ThemeProvider>
+					<Auth0Provider
+						domain='bishop.eu.auth0.com'
+						clientId='nKnGPNLE0ODtwFMZmV4Jhza2Xp6Rtjmx'
+						authorizationParams={{
+							redirect_uri: window.location.origin,
+						}}
+					>
+						<ThemeProvider theme={theme}>
+							<App />
+							<Toaster
+								position='top-right'
+								reverseOrder={false}
+								gutter={8}
+								toastOptions={{
+									duration: 5000,
+									style: {
+										background: '#fdfbea',
+										color: '#000000',
+									},
+								}}
+							/>
+							<GlobalStyle />
+						</ThemeProvider>
+					</Auth0Provider>
 				</BrowserRouter>
 			</PersistGate>
 		</Provider>
